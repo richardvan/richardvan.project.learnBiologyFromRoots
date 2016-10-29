fc341fc3d2b486bbed0f8cda16079bdb131cdb91
Richard Van <richardvan@Richards-MacBook-Air-2.local>
2016-10-29 09:26:15 -0700

    working scaffolding finding initial test data (20% of A, need to clean dictionary too)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # richardvan.project.learnBiologyFromRoots
 Look up roots of scientific terms with Greek or Latin origins
+
+## resources
+https://developers.google.com/sheets/quickstart/nodejs
+
+## (files) to not touch
+client_secret.json - is auth token to get access to google sheets, which are different by id
+EXAMPLE_googleSheets.js - last working (20161028) example
+
+## log entry
+
+### 2016-10-27
+started this project, figured it would be helpful to synthesis knowledge in a couple areas to create a tool that would be beneficial to the community moving forward.  Found a good dictionary (hard part) from an old book called "DICTIONARY of WORD ROOTS and COMBINING FORMS" b Donald J. Borror
+
+Many todos thought of:
+- clean up the dictionary
+- get a most recent 5 up
+- look up roots
+
+[initial test data]
+myostatin 
+allosome
+angiogenesis
+haplotype
+heterosis
--- index.js
+++ index.js
@@ -1,126 +1,199 @@
 
-//  initialize array of n-length
-//createArray();     // [] or new Array()
-//
-//createArray(2);    // new Array(2)
-//
-//createArray(3, 2); // [new Array(2),
-//                   //  new Array(2),
-//                   //  new Array(2)]
-function createArray(length) {
-    var arr = new Array(length || 0),
-        i = length;
+/////////////////////////////////////////////////////////
+// Getting the raw data from relative file path
+//////////////////////////
 
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
-    }
 
-    return arr;
+var fs = require('fs');
+var path = require('path');
+
+var filePath = path.join(__dirname, 'testData_letterB.input');
+var inputDictionaryToClean = "TODO_dictionary";
+
+inputDictionaryToClean = fs.readFileSync(filePath).toString().split("\n");
+//for(i in inputDictionaryToClean) {
+//    console.log(inputDictionaryToClean[i]);
+//}
+
+	
+//////////////////
+// MAIN body START
+console.log("************* START OF MAIN");
+//	console.log(" -- DEBUG:  findRoots('"+inputDictionaryToClean+"')");
+console.log(" *CALL: cleanRawDictionary -----");
+var dictionaryOfRootEntries = cleanRawDictionary(inputDictionaryToClean);
+
+
+console.log(" *CALL: findRoots -----");
+console.log(findRoots("abdicate",dictionaryOfRootEntries));
+console.log(findRoots("adolescence",dictionaryOfRootEntries));
+console.log(findRoots("actuary",dictionaryOfRootEntries));
+console.log(findRoots("acus",dictionaryOfRootEntries));					// TEST against =acus
+console.log(findRoots("agnus",dictionaryOfRootEntries));				// TEST against agn, -i, =us
+
+console.log("************* END OF MAIN");
+
+
+////////
+// NEW FUNCTIONS
+function DictionaryEntry(root,language,meaning) {
+    this._root = root;
+    this._language = language;
+    this._meaning = meaning;
+				
+    
+}
+function cleanRawDictionary(dictionaryToClean){
+	var returnValue = "TODO_cleanDictionaryReturnVALUE";
+	console.log (" -- DEBUG: dictionaryToClean START");
+	
+	// when copied, some longer entries finish as multi-entries, 
+	// change these to one line by knowing its multi if it 
+	// doesn't have a language which is delimited by (*)
+	// where * is a wildcard with possible abbreviations
+	
+	// ## abbreviations 
+	//	Af - African //	Ar - Arabi	//	AS - Anglo-Saxon //	Br - Brazilian
+	//	Ch - Chilean //	Dan - Danish //	Ε - English //	EI - East Indian
+	//	F - French //	Far - Faroese //	G - Greek //	Ger - German
+	//	Go - Gothlandic //	H - Hindustani //	Hb - Hebrew //	Ice - Icelandic
+	//	It - Italian //	L - Latin //	LL - Low Latin; Late Latin //	Mai - Malayan
+	//	Mex - Mexican //	ME - Middle English //	ML - Middle Latin //	My - Mythology
+	//	Ν - a proper name //	NL - New Latin //	OF - Old French //	OHG - Old High German
+	//	Pg - Portuguese //	Pp - Papuan //	Ps - Persian //	Pv - Peruvian
+	//	Rs - Russian //	SAm - South American //	Sp - Spanish //	Sw - Swedish
+	
+	var dictionaryFormattedAsEntries = [];
+	for (var key in dictionaryToClean)
+	{
+//  	console.log("key: %s, value: %s", key, dictionaryToClean[key])
+		
+		
+		// there are no * or «, misread as =
+		dictionaryToClean[key] = dictionaryToClean[key].replace(/[*«•]/g,'=');
+	
+
+	
+		// if it doesn't have (*), then add it to previous entry
+		var regExp = /\(([^)]+)\)/;
+		var matches = regExp.exec(dictionaryToClean[key]);
+		//		matches[1]  contains the value between the parentheses
+		//					  		otherwise null means it is multiline
+		if (matches == null || matches[1] == null)
+		{
+//			console.log("  - this is MULTILINE");
+			// previous value will be on top of list
+			var updatedPreviousValue = dictionaryFormattedAsEntries.pop();
+			updatedPreviousValue._meaning += dictionaryToClean[key];
+			dictionaryFormattedAsEntries.push(updatedPreviousValue);
+		}
+		else
+		{
+//			console.log(" matches[0]: " + matches[0]); 	// (G)
+//			console.log(" Language: " + matches[1]);		// G																			
+			var splits = dictionaryToClean[key].split(' ' + matches[0] + '. '); // split on ' (L). '
+//			console.log(splits); // [0] = root /w variability, [1] = meaning
+			var newEntry = new DictionaryEntry(splits[0],matches[1],splits[1]);
+													
+			// TODO LATER - have the language as abbreviation, convert later and display
+			dictionaryFormattedAsEntries.push(newEntry);
+		}
+	}
+	
+//	console.log (" -- DEBUG: dictionaryToClean after FIRST CLEAN");
+	console.log (dictionaryFormattedAsEntries);
+	console.log ("    LENGTH: " + dictionaryFormattedAsEntries.length);
+
+
+
+	console.log (" -- DEBUG: dictionaryToClean END");
+
+	// TODO - update this once it's further cleaned
+	returnValue = dictionaryFormattedAsEntries;
+	return returnValue;
 }
 
 
 
-/////////////////////////////////////////////////////////
-// Getting the data from a public accessible-editable spreadsheet
-//////////////////////////
- // ID of the Google Spreadsheet
- var spreadsheetIDallRoots = "1UFFKWNgjrsG-7PE-CLdndDw12-hl9WTvo0GChq61Ivw"; 
 
- var spreadsheetIDsavedRoots = "19YpAGS2kGPfWjZlyXEaYqLY5KavT7YAsPZ-ff_D3X3Y";
- 
-// initial main dictionary (2d array)
-var GLOBAL_dataFromSpreadSheet_allRoots = createArray(4, 4);
-var GLOBAL_dataFromSpreadSheet_savedRoots = createArray(2);
 
- // Make sure it is public or set to Anyone with link can view 
- var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDallRoots + "/od6/public/values?alt=json";
- 
- $.getJSON(url, function(data) {
-   
-   
-  var entry = data.feed.entry;
- 
-   var row=0,col=0;
- 
 
-     // _HEADER_
-     // _EXAMPLES (should be over 300)
-     // ['Root', 'Meaning', 'Origin', 'Example'], 
-     // ['a/n', 'not, without', 'Greek', 'abyss..'], 
-     // ['acro', 'top, height, tip, beginning', 'Greek', 'acrobat..'], 
-		
-					// HEADER -- comment if you don't want it
-//    var headerRowArray = new Array(4);
-//    headerRowArray[0] = 'Root_HEADER';
-//    headerRowArray[1] = 'Meaning_HEADER';
-//    headerRowArray[2] = 'Origin_HEADER';
-//    headerRowArray[3] = 'Example_HEADER';
-//    GLOBAL_dataFromSpreadSheet_allRoots[col++] = headerRowArray
-    
-    
-  $(entry).each(function(){
-    
-    
-    
-    // pull data from each row entry and insert it into multi-d array
-    var currentRowArray = new Array(4);
-    currentRowArray[0] = this.gsx$root.$t;
-    currentRowArray[1] = this.gsx$meaning.$t;
-    currentRowArray[2] = this.gsx$origin.$t;
-    currentRowArray[3] = this.gsx$example.$t;
-    GLOBAL_dataFromSpreadSheet_allRoots[col++] = currentRowArray;
-  });
- 
-   
-     
-			console.log("-- Debug: GLOBAL_dataFromSpreadSheet_allRoots");
-   console.log(GLOBAL_dataFromSpreadSheet_allRoots);
-      
-    // TODO - need inputOfWordToFindRoots
-//			 var inputOfWordToFindRoots = "myostatin";
-//			 var inputOfWordToFindRoots = "allosome ";
-//			 var inputOfWordToFindRoots = "angiogenesis";
-//			 var inputOfWordToFindRoots = "haplotype";
-			 var inputOfWordToFindRoots = "heterosis";
-			
-			console.log("-- Debug:  findRoots('"+inputOfWordToFindRoots+"')");
-			console.log("   Return: findRoots: " + findRoots(inputOfWordToFindRoots));
-			
- });
 /////////////////////////
 //							HELPER FUNCTIONS
 /////////////////////////
-function findRoots(wordToCheck) {
-		var returnValue = "findRootsReturnValue";
+
+//Short code - REGEX from http://stackoverflow.com/questions/26246601/wildcard-string-comparison-in-javascript
+function matchRuleShort(str, rule) {
+  return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
+}
+
+function findRoots(wordToCheck, dictionaryToCheckAgainst) {
+		var returnValue = 0;
 	
 		// TODO - look up all posibile comibinations of root on this, return all hits
 	
-	
-	// see if root is a subscript of wordToCheck
-	for (var i = 0, len = GLOBAL_dataFromSpreadSheet_allRoots.length; i < len; i++) {
-		var currentRow = GLOBAL_dataFromSpreadSheet_allRoots[i];
-		var currentRowCell0 = currentRow[0];
-		var currentRowCell1 = currentRow[1];
-		//console.log("-- [Checking] Root: " + currentCell);
-		
-	
-		
-		if (currentRowCell0.length > 1 && // skipping single letter
-						wordToCheck.indexOf(currentRowCell0) > -1 )
-		{
-			// TODO - [SPECIAL EXCEPTION] - letter a only, should only be searched against the first letter followed after it has been searched by other a[second letter] choices
-			// TODO - [SPECIAL EXCEPTION] - other single letter?
+		// see if root is a subscript of wordToCheck
+		var foundAtLeastOne = false;
+		for (var i = 0, len = dictionaryToCheckAgainst.length; i < len; i++) {
+				var currentObject = dictionaryToCheckAgainst[i];
+				var currentObjectRootToCheckAgainst = currentObject._root;
+				
+				// root might be a single word (ex. '=acus'
+																					// acus (possible on its own
+				if (currentObjectRootToCheckAgainst.charAt(0) == '=' ||
+						currentObjectRootToCheckAgainst.charAt(0) == '-' )
+					 
+				{
+					currentObjectRootToCheckAgainst = currentObjectRootToCheckAgainst.substring(1, currentObjectRootToCheckAgainst.length);
+				}
 			
-			console.log("- [FOUND subString!] " + currentRowCell0);
-			console.log("-   Meaning: " + currentRowCell1);
-			// TODO_BONUS - add in medical system too (depending if its full match?)
+				// root might have variations (ex.  'aeti, =a, -o' )
+																					//  aeti
+																					//  aetia (more likely because of =)
+																					//  aetio
+				var splits = currentObjectRootToCheckAgainst.split(', '); // split on ', '
+				var variations = "";
+				if (splits.length == 1)
+					variations = currentObjectRootToCheckAgainst;
+				else
+				{
+//					console.log ("--splits: " + splits);
+					variations = []; 	// one variation will always be the first term
+					variations.push (splits[0]);
+					
+//					console.log ("--variations (with splits): " + variations);
+					for (var j=1; j<splits.length; j++)
+					{
+						var currentVariation = splits[j];
+//						console.log("  [currentVariation] " + currentVariation);
+						variations.push (splits[0] + currentVariation.substring(1,currentVariation.length));
+					}
+				}
+			
+			
+//				console.log ("--finalVariations: " + variations);
+				// check each variation, at a minimum there will be one
+					
+			
+				if (currentObject._root.length > 1 && 								// skipping single letter
+						wordToCheck.indexOf(currentObjectRootToCheckAgainst) > -1 )
+				{
+					if (!foundAtLeastOne)
+					{
+						console.log("--[wordToCheck]: " + wordToCheck);					// display once
+						foundAtLeastOne = true;
+					}
+					console.log("-   [root]: " + currentObject._root);
+					console.log("-    [lang]: " + currentObject._language);
+					console.log("-    [meaning]: " + currentObject._meaning);
+					returnValue++;
+				}
+
 		}
-		
-	}
 	
 		//console.log(lookupRoot(wordToCheck));
     
+	returnValue = "----- Found " + returnValue + " match!\n";
 	
   return returnValue;
 }
