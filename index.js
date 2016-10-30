0286fc2efcf0671e5994d10c5945578399455c80
Richard Van <richardvan@Richards-MacBook-Air-2.local>
2016-10-29 19:30:37 -0700

    getting to work with heroku

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+//
+//// views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+//
+app.get('/', function(request, response) {
+  response.send ("learn biology project! - look in console");
+	
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
 
 /////////////////////////////////////////////////////////
 // Getting the raw data from relative file path
