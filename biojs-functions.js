78c247ca6ac315408436bf647a0544ef2436b8ba
Richard Van <richardvan@Richards-MacBook-Air-2.local>
2016-11-10 22:22:26 -0800

    seeing if pre-commit jshint works

---
+++ biojs-functions.js
@@ -0,0 +1,7 @@
+function biojs101(a,b) {
+    return a+b;
+}
+
+console.log(biojs101(1,2));
+
+module.exports = biojs101;
--- index.js
+++ index.js
@@ -1,60 +1,168 @@
-var express = require('express');
-var app = express();
-var json2html = require('node-json2html')
+var express = require('express');					// node framework for handling request/response
+																							// high-level api on top of [html][url]
+var json2html = require('node-json2html')	// node library
+																					// TODO: node library mongoose and mongodb setup
+																					// TODO: node library moment for date handling
+																	
+																					// TODO: node library jade or handlebars for templating
+// var sassMiddleware = require('node-sass-middleware'); // node library css preprocessor
 
-app.set('port', (process.env.PORT || 5000));
+var browserify = require('browserify-middleware');
+																					
+
+var app = express();
+
+
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('richardvan.project.learnBiologyFromRoots:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
 
 //app.use(express.static(__dirname + '/public'));
 //
 //// views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
-//
+app.get('/javascripts/bundle.js', browserify('./client/script.js'));	//browserify setup
+if (app.get('env') == 'development') {																//browser-sync setup
+  var browserSync = require('browser-sync');
+  var config = {
+    files: ["public/**/*.{js,css}", "client/*.js", "sass/**/*.scss", "views/**/*.jade"],
+    logLevel: 'debug',
+    logSnippet: false,
+    reloadDelay: 3000,
+    reloadOnRestart: true
+  };
+  var bs = browserSync(config);
+  app.use(require('connect-browser-sync')(bs));
+}
+
 app.get('/', function(request, response) {
-  console.log('/ called');
-	
-	
+  console.log('[calling..] /');
 	// create html on the fly from json object 
-	var html = 	'<h1>learn biology project!</h1>' +
-							'		<h2> How to use? </h2>' + 
-							'			<div>	type in /findRoots/[wordToLookUp] <div>';
-					
-							
-	
+	var html = 	'<h1>Learn biology project!</h1>' +
+							'		<h2> Root Finder 1.0 </h2>' + 
+							'			<div>	type in /findRoots/[wordToLookUp] <div>' +
+							'			<br>' +
+							'			<div>	TODO - put as textbox on this page <div>' +
+							'			<div>	TODO - GRE word in MongoDB?' +
+							'			<div>	TODO - GRE last 5 words' +
+							'			<div>	 <div>';
+		
   response.send (html);
-	
 });
 
-app.get('/test_acus', function(request, response) {
-	response.send (" way to return for /test-acus)");
-});
 app.get('/findRoots/:word', function(request, response) {
-		
+  console.log('[calling..] /findRoots/'+request.params.word);
+	
 	// create html on the fly from json object using JSON2HTML
 	//	 http://json2html.com/#tabs
-	var html = '<h2>'+request.params.word+'</h2>'
+	var html = '<h2>'+request.params.word+'</h2>';
 	
-	var transform = {"<>":"div","id":"${_variation}","html":[
-										{"<>":"div","html":"<b>${_variation}</b>"},
-										{"<>":"div","html":"${_root}"},
-										{"<>":"div","html":"[${_language}]"},
+	var transform = {"<>":"div","id":"${_variation}","style":"display: inline-block;margin-left: 30px","html":[
+										{"<>":"div","html":"<b>${_variation}</b> (${_root})"},
+										{"<>":"div","html":" <i>${_language}</i>"},
 										{"<>":"div","html":" ${_meaning}</i>"},
 										{"<>":"div","html":"<br>"}
-									]}
-
+									]};
+	
 	var roots = findRoots(request.params.word, dictionaryOfRootEntries);
 	html += json2html.transform(roots,transform);
 	
   response.send(html);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('/////////// ----');
-  console.log('//    Node app is running on port', app.get('port'));
-  console.log('/////////// ----');
 
+app.get('/learnWord/:word', function(request, response) {
+  console.log('[calling..] /learnWord/'+request.params.word);
+	
+
+  response.send(html);
 });
-
 
 
 
@@ -73,21 +181,61 @@ inputDictionaryToClean = fs.readFileSync(filePath).toString().split("\n");
 //    console.log(inputDictionaryToClean[i]);
 //}
 
+// ## abbreviations 
+var dictionaryOfAbbreviationsToLanguage = {
+	Af: 'African',
+	Ar: 'Arabic',
+	AS: 'Anglo-Saxon',
+	Br: 'Brazilian',
+	Ch: 'Chilean',
+	Dan: 'Danish',
+	E: 'English',
+	EI: 'East Indian',
+	F: 'French',
+	Far: 'Faroese',
+	G: 'Greek',
+	Ger: 'German',
+	Go: 'Gothlandic',
+	H: 'Hindustani',
+	Hb: 'Hebrew',
+	Ice: 'Icelandic',
+	It: 'Italian',
+	L: 'Latin',
+	LL: 'Low Latin; Late Latin',
+	Mai: 'Malayan',
+	Mex: 'Mexican',
+	ME: 'Middle English',
+	ML: 'Middle Latin',
+	My: 'Mythology',
+	N: 'a proper name',
+	NL: 'New Latin',
+	OF: 'Old French',
+	OHG: 'Old High German',
+	Pg: 'Portuguese',
+	Pp: 'Papuan',
+	Ps: 'Persian',
+	Pv: 'Peruvian',
+	Rs: 'Russian',
+	SAm: 'South American',
+	Sp: 'Spanish',
+	Sw: 'Swedish'
+}
+
 	
 //////////////////
 // MAIN body START
 console.log("************* START OF MAIN");
-//	console.log(" -- DEBUG:  findRoots('"+inputDictionaryToClean+"')");
-console.log(" *CALL: cleanRawDictionary -----");
+//	console.log(":- DEBUG: ' findRoots('"+inputDictionaryToClean+"')");
+console.log(" *CALL: 'cleanRawDictionary:----");
 var dictionaryOfRootEntries = cleanRawDictionary(inputDictionaryToClean);
 
 
-//console.log(" *CALL: findRoots -----");
+//console.log(" *CALL: 'findRoots:----");
 //console.log(findRoots("abdicate",dictionaryOfRootEntries));			// TEST basic test, one result
 //console.log(findRoots("adolescence",dictionaryOfRootEntries));
 //console.log(findRoots("actuary",dictionaryOfRootEntries));
 //console.log(findRoots("acus",dictionaryOfRootEntries));					// TEST against =acus
-//console.log(findRoots("agnus",dictionaryOfRootEntries));				// TEST against agn, -i, =us
+//console.log(findRoots("agnus",dictionaryOfRootEntries));				// TEST against agn,:i, =us
 //console.log(findRoots("myostatin",dictionaryOfRootEntries));				
 
 console.log("************* END OF MAIN");
@@ -106,28 +254,19 @@ function DictionaryEntry(root,language,meaning) {			// js object, treated like s
 
 function cleanRawDictionary(dictionaryToClean){
 	var returnValue = "TODO_cleanDictionaryReturnVALUE";
-	console.log (" -- DEBUG: dictionaryToClean START");
+	console.log (":- DEBUG: 'dictionaryToClean START");
 	
 	// when copied, some longer entries finish as multi-entries, 
 	// change these to one line by knowing its multi if it 
 	// doesn't have a language which is delimited by (*)
 	// where * is a wildcard with possible abbreviations
 	
-	// ## abbreviations 
-	//	Af - African //	Ar - Arabi	//	AS - Anglo-Saxon //	Br - Brazilian
-	//	Ch - Chilean //	Dan - Danish //	Ε - English //	EI - East Indian
-	//	F - French //	Far - Faroese //	G - Greek //	Ger - German
-	//	Go - Gothlandic //	H - Hindustani //	Hb - Hebrew //	Ice - Icelandic
-	//	It - Italian //	L - Latin //	LL - Low Latin; Late Latin //	Mai - Malayan
-	//	Mex - Mexican //	ME - Middle English //	ML - Middle Latin //	My - Mythology
-	//	Ν - a proper name //	NL - New Latin //	OF - Old French //	OHG - Old High German
-	//	Pg - Portuguese //	Pp - Papuan //	Ps - Persian //	Pv - Peruvian
-	//	Rs - Russian //	SAm - South American //	Sp - Spanish //	Sw - Swedish
+
 	
 	var dictionaryFormattedAsEntries = [];
 	for (var key in dictionaryToClean)
 	{
-//  	console.log("key: %s, value: %s", key, dictionaryToClean[key])
+//  	console.log("key: '%s, value: '%s", key, dictionaryToClean[key])
 		
 		// skip spaces
 		if (dictionaryToClean[key] == "")
@@ -149,7 +288,7 @@ function cleanRawDictionary(dictionaryToClean){
 		//					  		otherwise null means it is multiline
 		if (matches == null || matches[1] == null)
 		{
-//			console.log("  - this is MULTILINE");
+//			console.log(" : 'this is MULTILINE");
 			// previous value will be on top of list
 			var updatedPreviousValue = dictionaryFormattedAsEntries.pop();
 			updatedPreviousValue._meaning += dictionaryToClean[key];
@@ -157,29 +296,41 @@ function cleanRawDictionary(dictionaryToClean){
 		}
 		else
 		{
-//			console.log(" matches[0]: " + matches[0]); 	// (G)
-//			console.log(" Language: " + matches[1]);		// G																			
+//			console.log(" matches[0]: '" + matches[0]); 	// (G)
+//			console.log(" Language: '" + matches[1]);		// G																			
 			var splits = dictionaryToClean[key].split(' ' + matches[0] + '. '); // split on ' (L). '
 //			console.log(splits); // [0] = root /w variability, [1] = meaning
 			var newEntry = new DictionaryEntry(splits[0],matches[1],splits[1]);
 													
-			// TODO LATER - have the language as abbreviation, convert later and display
 			dictionaryFormattedAsEntries.push(newEntry);
 		}
 	}
 	
-//	console.log (" -- DEBUG: dictionaryToClean after FIRST CLEAN");
+//	console.log (":- DEBUG: 'dictionaryToClean after FIRST CLEAN");
 //	console.log (dictionaryFormattedAsEntries);
-	console.log ("    LENGTH: " + dictionaryFormattedAsEntries.length);
+	console.log ("    LENGTH: '" + dictionaryFormattedAsEntries.length);
 
 
 
-	console.log (" -- DEBUG: dictionaryToClean END");
+	console.log (":- DEBUG: 'dictionaryToClean END");
 
-	// TODO - update this once it's further cleaned
+	// TODO: 'update this once it's further cleaned
 	returnValue = dictionaryFormattedAsEntries;
 	return returnValue;
 }
+
+
+///// TODO
+// To further clean
+// 1.) -ineae (ending of *something)
+// 2.) anil, -t (L) old houese (G), creul
+// 3.) tor (G), creul
+// 4.) momol
+// 5.) camp (G) (L)
+// 6.) oxid
+// 6.) e/r (seee also /y 
+// 7.) *um
+// 7.) gens/gent
 
 
 
@@ -192,14 +343,14 @@ function cleanRawDictionary(dictionaryToClean){
 function findRoots(wordToCheck, dictionaryToCheckAgainst) {
 		var returnValue = [];
 	
-		// TODO - look up all posibile comibinations of root on this, return all hits
+		// TODO: 'look up all posibile comibinations of root on this, return all hits
 	
 		// see if root is a subscript of wordToCheck
 		var foundAtLeastOne = false;
 		for (var i = 0, len = dictionaryToCheckAgainst.length; i < len; i++) {
 				var curObj = dictionaryToCheckAgainst[i];
 				
-//				console.log("dictionary[i]: " + curObj._root);
+//				console.log("dictionary[i]: '" + curObj._root);
 				// root might be a single word (ex. '=acus'
 																					// acus (possible on its own
 				if (curObj._root.charAt(0) == '=' ||
@@ -209,14 +360,14 @@ function findRoots(wordToCheck, dictionaryToCheckAgainst) {
 					curObj._root = curObj._root.substring(1, curObj._root.length);
 				}
 			
-				// root might have variations (ex.  'aeti, =a, -o' )
+				// root might have variations (ex.  'aeti, =a,:o' )
 																					//  aeti
 																					//  aetia (more likely because of =)
 																					//  aetio
-				var variations = [];	
+				var variations = [];
 				var splits = curObj._root.split(', '); // split on ', '
 				
-//				console.log ("--splits: " + splits);
+//				console.log ("--splits: '" + splits);
 				
 				if (splits.length == 1)						// no variations
 					variations.push (curObj._root); 							
@@ -229,10 +380,21 @@ function findRoots(wordToCheck, dictionaryToCheckAgainst) {
 //						console.log("  [currentVariation] " + splits[0] + splits[j].substring(1,splits[j].length));
 						variations.push (splits[0] + splits[j].substring(1,splits[j].length));
 					}
+					
+					
+					/// DEBUG -switch in what root you are trying to look for
+//							if (curObj._root == "ne, -o"){
+//							console.log("-[wordToCheck]: '" + wordToCheck);	// display once
+//						console.log("-  [splists]: " + splits);
+//						console.log("-  [variations]: " + variations);
+//						console.log("-       [rozot]: " + curObj._root);
+//						console.log("-       [lang]: " + fullLanguage);
+//						console.log("-    [meaning]: " + curObj._meaning);
+//					}
 				}
 			
 			
-//				console.log ("--finalVariations: " + variations);
+//				console.log ("--finalVariations: '" + variations);
 			
 			
 				////////////////
@@ -242,22 +404,37 @@ function findRoots(wordToCheck, dictionaryToCheckAgainst) {
 				// check backwards so if there is a hit, it's more specific
 				for (var z=variations.length-1; z>=0 ; z--)
 				{
+//					if (variations[z] == 'neo'){
+//							console.log("-[wordToCheck]: '" + wordToCheck);	// display once
+//						console.log("-  [variation]: '" + variations[z]);
+//						console.log("-       [root]: '" + curObj._root);
+//						console.log("-       [lang]: '" + fullLanguage);
+//						console.log("-    [meaning]: '" + curObj._meaning);
+//					}
+					
 					if (variations[z].length > 1 && 											// skipping single letter
-							wordToCheck.indexOf(variations[z]) > -1 )					// is root or subscript of
+							wordToCheck.indexOf(variations[z]) >= 0 )					// is root or subscript of
 					{
 						if (!foundAtLeastOne)
 						{
-							console.log("-[wordToCheck]: " + wordToCheck);	// display once
+//							console.log("-[wordToCheck]: '" + wordToCheck);	// display once
 							foundAtLeastOne = true;
 						}
-						console.log("-  [variation]: " + variations[z]);
-						console.log("-       [root]: " + curObj._root);
-						console.log("-       [lang]: " + curObj._language);
-						console.log("-    [meaning]: " + curObj._meaning);
+						
+						
+						
+						// convert language abbreviation
+						var fullLanguage = dictionaryOfAbbreviationsToLanguage[curObj._language];
+//						console.log("-  [variation]: '" + variations[z]);
+//						console.log("-       [root]: '" + curObj._root);
+//						console.log("-       [lang]: '" + fullLanguage);
+//						console.log("-    [meaning]: '" + curObj._meaning);
+//						
+						
 						
 						var newHit = new DictionaryHitResponseObject (variations[z], 
 																													curObj._root,
-																													curObj._language,
+																													fullLanguage,		
 																													curObj._meaning);
 						
 						returnValue.push(newHit);
@@ -272,6 +449,7 @@ function findRoots(wordToCheck, dictionaryToCheckAgainst) {
 	
   return returnValue;
 }
+
 
 function DictionaryHitResponseObject(variation,root,language,meaning) {	
 		this._variation = variation;											// js object, treated like struct
